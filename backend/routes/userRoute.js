3480b187802787b6e17a26cb0fb139a056ba022d
Abeer <abeer.technology@gmail.com>
2022-04-06 13:39:32 +0600

    update password

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -5,7 +5,10 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  getUserDetails,
+  updatePassword,
 } = require("../controllers/userController");
+const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -17,5 +20,9 @@ router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
 module.exports = router;
