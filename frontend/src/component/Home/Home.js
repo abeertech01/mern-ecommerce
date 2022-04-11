a55a5ec984d70394c2b82cf0bcd37fb5b39fc2ec
Abeer <abeer.technology@gmail.com>
2022-04-11 21:36:01 +0600

    added meta data

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.css";
 import Product from "./Product.js";
+import MetaData from "../layout/MetaData";
 
 const product = {
   name: "Blue Tshirt",
@@ -13,6 +14,7 @@ const product = {
 const Home = () => {
   return (
     <Fragment>
+      <MetaData title=" ECOMMERCE" />
       <div className="banner">
         <p>Welcome to Ecommerce</p>
         <h1>FIND AMAZING PRODUCTS BELOW</h1>
