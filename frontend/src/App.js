b3f154b64d44796856c2133960f045ee78b353fb
Abeer <abeer.technology@gmail.com>
2022-04-11 21:26:20 +0600

    frontend starting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
 import Header from "./component/layout/Header/Header.js";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import webFont from "webfontloader";
 import Footer from "./component/layout/Footer/Footer.js";
+import Home from "./component/Home/Home.js";
 
 function App() {
   React.useEffect(() => {
@@ -16,6 +17,7 @@ function App() {
   return (
     <Router>
       <Header />
+      <Route exact path="/" component={Home} />
 
       <Footer />
     </Router>
