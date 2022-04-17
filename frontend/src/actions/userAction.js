0c2cf8a172d3d31ae7f024c91e641371ebe685e3
Abeer <abeer.technology@gmail.com>
2022-04-17 21:40:51 +0600

    load user in frontend

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -6,9 +6,13 @@ import {
   REGISTER_USER_REQUEST,
   REGISTER_USER_SUCCESS,
   REGISTER_USER_FAIL,
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOAD_USER_FAIL,
 } from "../constants/userConstants";
 import axios from "axios";
 
+// Login
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({ type: LOGIN_REQUEST });
@@ -27,6 +31,7 @@ export const login = (email, password) => async (dispatch) => {
   }
 };
 
+// Register
 export const register = (userData) => async (dispatch) => {
   try {
     dispatch({ type: REGISTER_USER_REQUEST });
@@ -41,6 +46,19 @@ export const register = (userData) => async (dispatch) => {
       type: REGISTER_USER_FAIL,
       payload: error.response.data.message,
     });
+  }
+};
+
+// Load User
+export const loadUser = () => async (dispatch) => {
+  try {
+    dispatch({ type: LOAD_USER_REQUEST });
+
+    const { data } = await axios.get(`api/v1/me`);
+
+    dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({ type: LOAD_USER_FAIL, payload: error.response.data.message });
   }
 };
 
